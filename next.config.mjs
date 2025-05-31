6457c945d5300a24bf139dfad6f164635e801a54
aydamacink <dayanamick@gmail.com>
2025-05-31 22:50:38 +0200

    Revert "Implement token price fetching from 1inch API (not working yet) and update TopHoldingsChart for enhanced data processing (also not working yet)"
    This reverts commit 2e01c0d66cec4f2727af759c6fcf1b5a2d339495.

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/1inch-api/:path*',
-        destination: 'https://api.1inch.dev/:path*',
-        basePath: false
-      }
-    ];
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
